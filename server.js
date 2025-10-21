f768cd6ebcbcf317219a74d9f14cdd7fdc829c56
Your Name <your.email@example.com>
2025-10-21 14:01:35 +0700

    Ready for deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Connect to MongoDB
